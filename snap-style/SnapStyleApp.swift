13b545f2338d380fd1b3b375a497753b6f9e949e
David Mahbubi <ulrichdavid0370@gmail.com>
2023-06-27 13:39:34 +0700

    Initial Commit

---
+++ snap-style/SnapStyleApp.swift
@@ -0,0 +1,20 @@
+//
+//  snap_styleApp.swift
+//  snap-style
+//
+//  Created by David Mahbubi on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SnapStyleApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
