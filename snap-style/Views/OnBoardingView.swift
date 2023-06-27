838ba343a04cb983b8315a9c09dfc904e73c3ce1
Denny Chandra <dennychandrawijaya@gmail.com>
2023-06-27 14:07:13 +0700

    feat: onboardingview

---
+++ snap-style/Views/OnBoardingView.swift
@@ -0,0 +1,42 @@
+//
+//  OnBoardingView.swift
+//  snap-style
+//
+//  Created by Denny Chandra Wijaya on 23/06/23.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    @State  var increment: Int = 0
+    var body: some View {
+        //        SecondBoarding()
+        NavigationStack{
+            
+            VStack {
+                if(increment == 0){
+                    
+                    OnBoardingFirstView(increment:$increment)
+                }else if(increment == 1){
+                    
+                        OnBoardingSecondView(increment:$increment)
+                }
+                else{
+                    OnBoardingThirdView(increment:$increment)
+                }
+            }
+        }
+    }
+}
+
+struct OnBoardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardingView()
+    }
+}
+
+
+
+
+
+
