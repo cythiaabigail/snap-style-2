c5d5c9ea0564df0954936a9033ff9c3cf09f6dde
Denny Chandra <dennychandrawijaya@gmail.com>
2023-07-12 17:50:53 +0700

    update: bodycalculation

--- snap-style/ViewModels/BodyCalculation.swift
+++ snap-style/ViewModels/BodyCalculation.swift
@@ -7,7 +7,10 @@
 
 import SwiftUI
 
+
 class BodyCalculation {
+    @AppStorage("favorites") var favorites: String = "World"
+    
     let image : UIImage
     let fixedTall : CGFloat
     let secondImage: UIImage
@@ -17,7 +20,7 @@ class BodyCalculation {
         self.fixedTall = fixedTall
         self.secondImage = secondImage
     }
-
+    
     func imagePixelPersize(humanJoints: [String: CGPoint], isFront: Bool) -> CGFloat {
         guard let headHumanJoint = humanJoints["neck_1_joint"] else {return 0.0}
         guard let feetHumanJoint = humanJoints["left_foot_joint"] else {return 0.0}
@@ -35,16 +38,23 @@ class BodyCalculation {
     func bodyAroundCalculation(humanJoinstFront : [String : CGPoint], humanJoinstSide: [String: CGPoint]) -> [String: CGFloat] {
         var bodyAround : [String: CGFloat] = [:]
         
+        var bodyShape : String = ""
+        
         let bodyPerPixelFront = imagePixelPersize(humanJoints: humanJoinstFront, isFront: true)
         let bodyPerPixelSide = imagePixelPersize(humanJoints: humanJoinstSide, isFront: false)
         
         bodyAround["shoulder"] = aroundBody(humanJoinstFront: humanJoinstFront, humanJoinstSide: humanJoinstSide, bodyPerPixelFront: bodyPerPixelFront, bodyPerPixelSide: bodyPerPixelSide, leftSide: "left_shoulder_1_joint", rightSide: "right_shoulder_1_joint")
         bodyAround["hip"] = aroundBody(humanJoinstFront: humanJoinstFront, humanJoinstSide: humanJoinstSide, bodyPerPixelFront: bodyPerPixelFront, bodyPerPixelSide: bodyPerPixelSide, leftSide: "left_upLeg_joint", rightSide: "right_upLeg_joint")
-        print(humanJoinstSide["up_hip_left"])
+        //        print(humanJoinstSide["up_hip_left"])
         bodyAround["up_hip"] = aroundBody(humanJoinstFront: humanJoinstFront, humanJoinstSide: humanJoinstSide, bodyPerPixelFront: bodyPerPixelFront, bodyPerPixelSide: bodyPerPixelSide, leftSide: "up_hip_left", rightSide: "up_hip_right")
         bodyAround["bust"] = aroundBody(humanJoinstFront: humanJoinstFront, humanJoinstSide: humanJoinstSide, bodyPerPixelFront: bodyPerPixelFront, bodyPerPixelSide: bodyPerPixelSide, leftSide: "bust_left", rightSide: "bust_right")
+        
+        bodyShape = bodyShapeResult(fourData: bodyAround, marginError: 0.05)
+        
         print(bodyAround)
-    
+        print(bodyShape)
+        
+        
         return bodyAround
     }
     
@@ -57,5 +67,177 @@ class BodyCalculation {
     
     private func elipseCalculation(bodyRangeSide : CGFloat, bodyRangeFront: CGFloat) -> CGFloat {
         return (bodyRangeSide + bodyRangeFront) * 22 / 14
+    }
+    
+    func bodyShapeResult(fourData : [String: CGFloat], marginError : CGFloat) -> String{
+        
+        //data chytia || rectangle
+        let shoulder = 103.0
+                        let hip = 92.0
+        let waist = 107.0
+                        let bust = 83.0
+        
+        // data hourglass
+        //                let shoulder = 89.5
+        //                let hip = 90.0
+        //                let waist = 79.0
+        //                let bust = 90.0
+        
+        //data inv triangle
+        //        let shoulder = 100.0
+        //        let hip = 80.0
+        //        let waist = 69.0
+        //        let bust = 90.0
+        
+        //data apple
+        //        let shoulder = 89.5
+        //        let hip = 90.0
+        //        let waist = 79.0
+        //        let bust = 100.0
+        
+        //data pear
+//        let shoulder = 89.5
+//        let hip = 100.0
+//        let waist = 90.0
+//        let bust = 90.0
+        
+        
+        
+        let rangeShoulderDown = shoulder - (marginError * shoulder)
+        let rangeShoulderUp = shoulder + (marginError * shoulder)
+        
+        let rangeShoulderDownTenPercent = shoulder - (0.1 * shoulder)
+        let rangeShoulderUpTenPercent = shoulder + (0.1 * shoulder)
+        
+        let rangeHipDown = hip - (marginError * hip)
+        let rangeHipUp = hip + (marginError * hip)
+        
+        let rangeWaistDown = waist - (marginError * waist)
+        let rangeWaistUp = waist + (marginError * waist)
+        
+        let rangeBustDown = bust - (marginError * bust)
+        let rangeBustUp = bust + (marginError * bust)
+        
+        let gender = "Male"
+        
+        var hasil = "Unidentified"
+        //        print(rangeShoulderDown)
+        //        print(rangeShoulderUp)
+        //        print(rangeHipDown)
+        //        print(rangeHipUp)
+        //        print(rangeWaistDown)
+        //        print(rangeWaistUp)
+//        print(rangeBustDown)
+//        print(rangeBustUp)
+        
+        //FEMALE
+        if(gender == "Female"){
+            //Rectangle,
+            //Shoulder dan Hips memiliki ukuran yang hampir sama
+            //Bust lebih kecil dari shoulder/hips
+            //Waist lebar paling kecil, tapi perbandingan dari bust, gaboleh lebih kecil sampe 20cm
+            if(hip >= rangeShoulderDown && hip <= rangeShoulderUp)
+            {
+                if(bust <= shoulder || bust <= hip){
+                    if(waist <= bust && bust - waist < 20.0){
+                        hasil = "Rectangle"
+                        
+                    }
+                }
+            }
+            
+            
+            //HourGlass,
+            //Bust and Hips memiliki ukuran hampir sama
+            //Waist kecil banget, harus lebih dari 20cm perbedaannya
+            if(hip >= rangeBustDown && hip <= rangeBustUp)
+            {
+                if(waist < hip && hip - waist >= 10.0){
+                    hasil = "HourGlass"
+                }
+            }
+            
+            //Inverted Triangle
+            //Shoulder paling lebar
+            //Bust mengecil
+            //Hips mengecil
+            if(shoulder>hip && shoulder>bust && shoulder>waist){
+                if(bust > hip && bust > waist){
+                    hasil = "Inverted Triangle"
+                    
+                }
+            }
+            
+            //Apple,
+            //Bust paling besar
+            //hips lebih kecil
+            if(bust > rangeHipUp && bust > rangeWaistUp && bust > rangeShoulderUp){
+                hasil = "Apple"
+            }
+            
+            //Pear
+            //Hips paling besar
+            if(hip > rangeBustUp && hip > rangeWaistUp && hip > rangeShoulderUp){
+                hasil = "Pear"
+            }
+        }
+        else if(gender == "Male"){
+            //Rectangle
+            //shoulder, waist, hips mirip2
+            if(hip >= rangeShoulderDown && hip <= rangeShoulderUp)
+            {
+                if(bust <= shoulder || bust <= hip){
+                    if(waist <= bust && bust - waist < 20.0){
+                        hasil = "Rectangle"
+                        
+                    }
+                }
+            }
+            if(waist > bust && waist > shoulder && waist > hip){
+                if(shoulder >= rangeWaistDown && shoulder <= rangeWaistUp){
+                    hasil = "Oval"
+                }else
+                {
+                    hasil = "Triangle"
+                }
+            }
+            if(shoulder > rangeHipUp && shoulder > rangeWaistUp && shoulder > rangeBustUp){
+                if(rangeShoulderDownTenPercent >= hip && rangeShoulderDown >= waist){
+                    hasil = "Inverted Triangle"
+                }else{
+                    hasil = "Trapezoid"
+                }
+            }
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //COWOK
+        //Rectangle
+        //shoulder, waist, hips mirip
+        
+        //Oval
+        //Shoulder & hips lebih kecil dibanding waist
+        
+        //triangle
+        //shoulder paling kecil
+        
+        //invert triangle
+        //Shoulder paling lebar
+        //waist dan hip mirip2
+        
+        //trapezoid
+        //shoulder paling lebar
+        //waist dan hip mirip2 sama shoulder
+        
+        return hasil
     }
 }
